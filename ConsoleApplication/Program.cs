14e9c62a7b34900d42154872f38bce8b2f1e91fc
aleksankaa <keksikzh@gmail.com>
2024-06-16 19:46:55 +0400

    lla

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,18 @@
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            cl cl = new cl();
+            cl.AddObjectTocl("табуретка");
+            cl.AddObjectTocl("Тухлые яблоки");
+            cl.clearcl();
+            cl.AddObjectTocl("");
+            cl.AddObjectTocl("");
+                        cl.AddObjectTocl("");
+                        cl.AddObjectTocl("");
+            
+        }
+    }
+}
